6286167d0606749e6e6b6973d0f3ebbb1cdc3756
Nicolas Bessin <nicolasbessin49@gmail.com>
2023-10-12 21:25:16 +0200

    Updated the constants

--- python/constants.py
+++ python/constants.py
@@ -1,43 +1,78 @@
-# INSTANCE CONSTANTS
-# Encompasses all categories in instance
-PARAMETERS = "parameters"
+# INPUT PARAMETERS NAMES
 
-# Size parameters
-SIZE = "size"
-NB_JOBS = "nb_jobs"
-NB_TASKS = "nb_tasks"
-NB_MACHINES = "nb_machines"
-NB_OPERATORS = "nb_operators"
+# General parameters
+GEN_PARAMETERS = "general_parameters"
+MAX_POWER = "maximum_power"
+CURTAILING_COST = "curtailing_cost"
+CURTAILING_PENA = "curtailing_penalty"
+MAX_CURTAILING = "maximum_curtailing"
+FIX_COST_CABLE = "fixed_cost_cable"  # Cost of the turbine - substation cables
+VAR_COST_CABLE = "variable_cost_cable"  # Cost of turbine - substation cables
+MAIN_LAND_STATION = "main_land_sation"
 
-# Costs parameters
-COSTS = "costs"
-UNIT_PENALTY = "unit_penalty"
-TARDINESS = "tardiness"
+# Position parameters
+X = "x"
+Y = "y"
 
-# Jobs
-JOBS = "jobs"
-JOB = "job"
-SEQUENCE = "sequence"
-RELEASE_DATE = "release_date"
-DUE_DATE = "due_date"
-WEIGHT = "weight"
+# Substation parameters
+SUBSTA_TYPES = "substation_types"
+ID = "id"
+COST = "cost"
+RATING = "rating"
+PROB_FAIL = "probability_of_failure"
 
-# Tasks
-TASKS = "tasks"
-TASK = "task"
-PROCESSING_TIME = "processing_time"
+# Land-Substation cables parameters
+LAND_SUB_CABLE_TYPES = "land_substation_cable_types"
+# Already defined, same as before
+# (if need be, might separate ids and others by type later)
+# ID = "id"
+FIX_COST = "fixed_cost"
+VAR_COST = "variable_cost"
+# PROB_FAIL = "probability_of_failure"
+# RATING  = "rating"
 
-# Machines
-MACHINES = "machines"
-MACHINE = "machine"
-OPERATORS = "operators"
+# Substation-Substation cable parameters
+SUB_SUB_CABLE_TYPES = "substation_substation_cable_types"
+# SAME INTERNAL PARAMETERS AS LAND_SUB_TYPES
+# ID = "id"
+# FIX_COST = "fixed_cost"
+# VAR_COST = "variable_cost"
+# PROB_FAIL = "probability_of_failure"
+# RATING  = "rating"
 
-# SOLUTION CONSTANTS
-START = "start"
-OPERATOR = "operator"
+# Subtation location parameters
+# ID = "id"
+# X = "x"
+# Y = "y"
+
+# wind turbines parameters
+# ID = "id"
+# X = "x"
+# Y = "y"
+
+# Scenarios parameters
+WIND_SCENARIO = "wind_scenario"
+# ID = "id"
+PROBABILITY = "probability"
+POWER_GENERATION = "power_generation"
+
+
+# OUTPUT PARAMETERS NAMES
+
+SUBSTATIONS = "substations"
+# ID = "id"
+SUB_TYPE = "substation_type"
+LAND_CABLE_TYPE = "land_cable_type"
+LINKED_TO_ANOTHER = "linked_to_another"
+LINKED_SUB_ID = "linked_substation_id"
+LINKED_CABLE_TYPE = "linked_cable_type"
+
+TURBINES = "turbines"
+# ID = "id"
+SUBSTATION_ID = "substation_id"
 
 
 # Keys needed in solutions
-KEYS_EXPECTED = [TASK, START, MACHINE, OPERATOR]
+KEYS_EXPECTED = []
 # Maximum size of values in solution
-MATCHING = {TASK: NB_TASKS, OPERATOR: NB_OPERATORS, MACHINE: NB_MACHINES}
+MATCHING = {}
