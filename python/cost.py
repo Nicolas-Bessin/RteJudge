e785463cb0ec302f5d7b3c4f4e4cdcde38d170af
adcrry <ducorroyalexandre@gmail.com>
2023-11-08 22:55:26 +0100

    fix issue with dist

--- python/cost.py
+++ python/cost.py
@@ -1,6 +1,10 @@
 from constants import *
-from math import dist
+from math import sqrt
 from errors import InstanceError
+
+
+def dist(a, b):
+    return sqrt((a[0] - b[0]**2 + (a[1] - b[1])**2))
 
 
 def cost(instance, solution):
